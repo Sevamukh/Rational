d90ec50bd83f699de691b6fd94b24df903332761
Vsevolod Mukhin <sevamukh@gmail.com>
2023-03-29 12:26:22 +0700

    rename test suite class

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -1,7 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class Tests {
+public class RationalTest {
     @Test
     public void testStandardConstructor() {
         Rational standard = new Rational();
